1cecd65f32117f9b8cb794bc6dc7075828813809
Tapio Räsänen <wandrw4@gmail.com>
2014-12-16 19:13:09 +0200

    Calculate returns
    Calculated log returns and compared it to stop-loss limit.

--- Scripts/TestAlgorithm.r
+++ Scripts/TestAlgorithm.r
@@ -1,5 +1,6 @@
 # Use data created with Intervals.sas to predict price (with N = small)
 # Use sample data if no real tick data is available
+# Use previous day's data to train model
 
 tickdata <- read.csv('C:/myfolders/StockData/intervaldata1.csv')
 
@@ -9,7 +10,7 @@ tickdata <- read.csv('C:/myfolders/StockData/intervaldata1.csv')
 # parameters:
 param_limit <- 0.05				# stop-loss limit
 param_ci <- 0.95				# confidence intervals
-param_buy <- 13.15				# buy price/current price
+param_buy <- 13.00				# buy price/current price
 param_adjust_buy <- 0.005		# price adjustment for current price
 param_n <- 10					# forecast n.ahead
 
@@ -27,15 +28,20 @@ tickseries <- as.ts(tickdata_c[-1,-4])
 
 # Try vector autoregression 
 
-tickvar1 <- VAR(tickseries, p = 2, type = "none")
+tickvar1 <- VAR(tickseries, p = 3, type = "none")
 #VAR(tickseries, p = 2, type = "const")
 #tickvar1 <- VAR(tickseries, p = 2, type = "trend")
 #tickvar1 <- VAR(tickseries, p = 2, type = "both")
 
-plot(tickvar1)
+#plot(tickvar1)
+AIC(tickvar1)
 
 tickvar1p <- predict(tickvar1, n.ahead = param_n, ci = param_ci)
 fanchart(tickvar1p)
+
+# Residuals looking good...
+plot(1:10, resid1, type='l')
+abline(h=0)
 
 # Forecasted values for price
 dim(tickvar1p$fcst$log_price)
@@ -50,9 +56,12 @@ resid1 <- tickdata$Price[101:110] - exp(tickvar1p$fcst$log_price[,1])
 # Lower 5% limit
 exp(tickvar1p$fcst$log_price[,1] - tickvar1p$fcst$log_price[,4])
 
-# Looking good...
-plot(1:10, resid1, type='l')
-abline(h=0)
+# Returns with param_buy price
+returns <- exp(tickvar1p$fcst$log_price[,1] - tickvar1p$fcst$log_price[,4]) - rep(param_buy,10)
+
+min(returns) < -param_limit # FALSE: Does not exceed stop-loss limit, but:
+
+plot(1:10, returns)
 
 # predict log returns and risk n.ahead = param_n with ci = param_ci
 
